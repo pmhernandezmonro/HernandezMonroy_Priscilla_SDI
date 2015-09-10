a59043fae2973cb1ed32df6ece387bbc000021c5
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-10 11:55:25 -0400

    Prompted user for name
    Prompted user for name

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -6,3 +6,11 @@ GoTo Training Week #2
  */
 
 
+
+// Create an age Calculator
+
+//Ask the user for their name
+var name= prompt("Let's figure out how old you are!\nPlease type in your name.");
+
+//Use the variable and say hello
+alert("Welcome "+name+"!");
