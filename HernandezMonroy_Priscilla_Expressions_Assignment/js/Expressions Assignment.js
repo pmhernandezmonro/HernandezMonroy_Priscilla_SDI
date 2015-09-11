ed18048d5ad5c10ec5d39c4d47ff884dfc74a20a
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-10 20:49:10 -0400

    Adding more code
    Adding code for child care calculator.

---
+++ HernandezMonroy_Priscilla_Expressions_Assignment/js/Expressions Assignment.js
@@ -0,0 +1,17 @@
+/*
+ Priscilla Hernandez-Monroy
+ SDI Section #3
+ Expressions Assignment
+ 9-10-2015
+ */
+
+//alert ("testing.");
+
+// Create a child care calculator
+
+//Greet the user
+var greeting = prompt("Welcome!\n Lets find out how much you spend on child care in one year.");
+
+//Ask user how much do they spend on child care on a monthly bases
+
+var question = prompt("How much do you spend on child care on a monthly bases?");
