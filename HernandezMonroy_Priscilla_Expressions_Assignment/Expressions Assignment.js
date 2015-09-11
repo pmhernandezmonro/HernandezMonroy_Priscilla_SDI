b798854b12dae7ed6dc4f88c4fce384ad0c9d1ce
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-10 20:04:30 -0400

    creating calculator
    creating calculator on how much a person would spend on child care in
    one year.

---
+++ HernandezMonroy_Priscilla_Expressions_Assignment/Expressions Assignment.js
@@ -0,0 +1,14 @@
+/*
+ Priscilla Hernandez-Monroy
+ SDI Section #3
+ Expressions Assignment
+ 9-10-2015
+ */
+
+
+
+//Create a child care calculator
+
+//Ask user for their name
+var name= prompt("Would you like to figure out how much you spend on child care in one year? ");
+
