4589220c92a29112ac5d028309c22c54c959104a
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-10 21:15:26 -0400

    Creating code for calculations
    Created a code to help user calculate how much they spend on child care
    in one month.

--- HernandezMonroy_Priscilla_Expressions_Assignment/js/Expressions Assignment.js
+++ HernandezMonroy_Priscilla_Expressions_Assignment/js/Expressions Assignment.js
@@ -7,11 +7,24 @@
 
 //alert ("testing.");
 
-// Create a child care calculator
+//Create a child care calculator
 
 //Greet the user
-var greeting = prompt("Welcome!\n Lets find out how much you spend on child care in one year.");
+alert("Welcome!\nLets find out how much you spend on child care in one year.");
 
 //Ask user how much do they spend on child care on a monthly bases
 
-var question = prompt("How much do you spend on child care on a monthly bases?");
+var monthlyBases = prompt("How much do you spend on child care on a monthly bases?");
+
+//Create console.log to check answer
+console.log(monthlyBases);
+
+//Need to know much they spend in one month on child care
+
+//Find out how much they pay for child care in 4 weeks
+var Weeks = 4;
+
+//Figure out amount
+var amount = Weeks * monthlyBases;
+
+
