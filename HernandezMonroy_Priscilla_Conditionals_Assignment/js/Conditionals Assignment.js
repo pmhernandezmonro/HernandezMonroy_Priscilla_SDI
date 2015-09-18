836a027ccecf45c7ee92ec30b086f58fb0207057
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-17 22:38:42 -0400

    created console.log
    Created console.log to help determine how many hours user must visit
    park.

--- HernandezMonroy_Priscilla_Conditionals_Assignment/js/Conditionals Assignment.js
+++ HernandezMonroy_Priscilla_Conditionals_Assignment/js/Conditionals Assignment.js
@@ -20,18 +20,23 @@ goal = prompt("Hello we are going to calculate if have reached your goal of vist
 if (goal == "" ) {//Alert user if they leave prompt blank.}
     goal = prompt("Whoops! Please do not leave blank. Enter now");
 }
+//Ask user to enter how many hours they vist park.
 hoursPerDay = prompt("Please enter how many hours you visit the park per day ");
 if (hoursPerDay == "") {//Alert user if they leave prompt blank.}
     hoursPerDay = prompt("Whoops! Please do not leave blank. Enter now");
 }
+//Ask user how many days out of the week they visit park.
 days = prompt("How many days do you visit the park per week? ");
 if (days == "") {//Alert user if they leave prompt blank.}
     days = prompt("Whoops! Please do not leave blank. Enter now");
 }
+//Determine how total hours user should be going to park
+totalHours = Number (goal) - Number(hoursPerDay) * Number (days);
+console.log("Looks like you are" + " "+totalHours+ " " + "hour from acheving your goal of riding your bike at the park. ");
 
+//Let user know if they achieved their goal for the week
+if (goal > totalHours && totalHours == 0) {
 
-
-
-
-
-
+}else{
+    console.log("")
+}
