317f6b33cdd28fe6f2b50acdb683e67544f0cb3c
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-17 20:55:59 -0400

    Adding prompts
    Added two prompts for user to respond to.

--- HernandezMonroy_Priscilla_Conditionals_Assignment/js/Conditionals Assignment.js
+++ HernandezMonroy_Priscilla_Conditionals_Assignment/js/Conditionals Assignment.js
@@ -7,4 +7,19 @@ Conditionals Assignment
 
 //alert ("Testing to see if connected.");
 
+//Create a calculator to determine whether or not I should go to the park
+
+//Greet user
+
+alert("Hello! Should you go to the park today? Lets find out ");
+
+//Ask user to enter outside temp
+var currentTemp = prompt("Please enter outside temp.");
+
+//Now ask user limit of temp to tolerate
+var maxTemp = prompt("Please enter the max temp you would tolerate today.");
+
+
+
+
 
