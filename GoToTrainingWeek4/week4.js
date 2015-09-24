c475125c7053998a6ecf4296590087fa2e434a39
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-24 14:31:22 -0400

    prompted for max
    word validation

--- GoToTrainingWeek4/week4.js
+++ GoToTrainingWeek4/week4.js
@@ -8,3 +8,59 @@ GoTo Training Week 4
 
 //alert("Testing")
 
+ //Create a fle that will give us a random number between two numbers.
+
+ //Ask the user for the min number.
+ var minNum = prompt("Let's find a number between two numbers!\nWhat is the min number:");
+//Validate the prompt is not blank and it is a number
+while(minNum==="" || isNaN(minNum)){
+    //re-prompt the user
+    if(minNum===""){
+        minNum = prompt("Please do not leave blank and only use numbers.\nWhats is the min number:");
+
+
+
+
+
+
+    }else if(isNaN(minNum))
+
+    minNum = prompt("PLease do not leave blank.\nWhats is the min number:");
+
+
+}
+
+
+ /*
+ Basic structure of a while loop.
+
+ //Declare a variable
+ var x = prompt("Type in a number");
+
+ while( x < 6){ //condition to test
+
+    //Some sort of change to the variable we in our condition to test.
+     x++;
+ }
+
+  */
+
+ //prompt user for max number
+ var maxNum = prompt("What is the max number:");
+ //Validate directly after the prompt.
+ while(maxNum==="" || isNaN(maxNum)){
+
+     maxNum= prompt("Please do not leave blank and only use number.\nWhat is the max value:");
+
+ }
+
+ //What if you wanted to validate words
+ //Ask a yes or no question
+ var happy = prompt("Are you happy today?");
+ //convert to all lowercase
+happy = happy.toLowerCase();
+
+ while(happy !="yes"){
+     happy = prompt("Please only yes or no.\nAre you happy today?");
+
+ }
