16ce2e02b56f83b442700e5f990fa01bc018dfa1
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-24 19:11:44 -0400

    fixed error with variable
    Wasn’t getting total number for perimeter fixed var.

--- HernandezMonroy_Priscilla_Functions_Assignment/js/Functions_Assignment.js
+++ HernandezMonroy_Priscilla_Functions_Assignment/js/Functions_Assignment.js
@@ -7,22 +7,35 @@ Functions Assignment
  */
 
 //alert("Testing for connection.");
-//Creating calculator for use to find perimeter and area of square.
+//Creating calculator to find perimeter and area of square.
 
- function perimeter(l,h,w){
-     //find the perimeter of a square it is 4 + 8 + l + w
-     var perimeter = 4+8+l+w;
+ function perimeter (l,w){
+     //find the perimeter of a square is l + l + w + w
+     var perimeter = Number(l) + Number(l) + Number(w) + Number(w);
      return perimeter;
-
  }
+
 //Prompt user for length and width
  var l = prompt("Enter the length of the square.");
  //If user leaves blank ask again.
- while(w==="" || isNaN(l)){
+ while(l==="" || isNaN(l)){
      //Prompt the user again.
      l=prompt("Please do not leave blank.\n Enter the length of the square.");
-     
-
 
 
  }
+ //Validate if user enters a number and not a word.
+ var w = prompt("Enter the width of the square.");
+ //If user leaves blank ask again.
+ while(w==="" || isNaN(w)) {
+     //Prompt the user again.
+     w = prompt("Please do not leave blank.\n Enter the width of the square as a number.");
+ }
+//Give user perimeter
+         var total = perimeter(l, w);
+         console.log("The total perimeter of a square with a " +l+ " and a width of " +w+ " is " +total+ ".");
+
+
+
+
+
