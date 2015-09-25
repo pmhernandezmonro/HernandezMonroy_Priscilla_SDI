6222fd03a321b9bc6cc5f21008a82046de8c7283
pmhernandezmonro <Priscilla@Priscillas-MacBook-Pro.local>
2015-09-24 20:07:55 -0400

    Added prompt to get total area.
    Added a prompt to ask use to enter a number for l and w to receive the
    area of a square.

--- HernandezMonroy_Priscilla_Functions_Assignment/js/Functions_Assignment.js
+++ HernandezMonroy_Priscilla_Functions_Assignment/js/Functions_Assignment.js
@@ -25,17 +25,46 @@ Functions Assignment
 
  }
  //Validate if user enters a number and not a word.
- var w = prompt("Enter the width of the square.");
+ var w = prompt("Enter the width of the square as a number.");
  //If user leaves blank ask again.
  while(w==="" || isNaN(w)) {
      //Prompt the user again.
      w = prompt("Please do not leave blank.\n Enter the width of the square as a number.");
  }
 //Give user perimeter
-         var total = perimeter(l, w);
-         console.log("The total perimeter of a square with a " +l+ " and a width of " +w+ " is " +total+ ".");
+         var totalPerimeter = perimeter(l, w);
+         console.log("The total perimeter of a square with a " +l+ " and a width of " +w+ " is " +totalPerimeter+ ".");
 
 
+function area (l,w){
+    //find area of square l * w
+    var area = Number(l) * Number(w);
+    return area;
+}
 
+ //Prompt user for length and width to find area of square.
+alert("Now we will find the area of the square.");
+
+ var l = prompt("Please enter the length of the square to find area.");
+ //If user leaves blank ask again.
+ while(l==="" || isNaN(l)){
+     //Prompt the user again.
+     l=prompt("Please do not leave blank.\n Enter the length of the square to find area.");
+
+
+ }
+ //Validate if user enters a number and not a word.
+ var w = prompt("Enter the width of the square .");
+ //If user leaves blank ask again.
+ while(w==="" || isNaN(w)) {
+
+     //Prompt the user again.
+     w = prompt("Please do not leave blank.\n Enter the width of the square as a number.");
+ }
+//Give user area of square
+     var totalArea = area(l, w);
+     console.log("The total area of a square with a " +l+ " and a width of " +w+ " is " +totalArea+ ".");
+
+ 
 
 
